464d4c85d16b6ca8ad85a373c089d35700e261f6
adrianfile-tactcia <adrianf@tacticasoft.net>
2026-04-16 13:35:08 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,7 @@
-const CACHE = 'app-siestas-v2'; // cambiá el número cuando actualices
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE).then((cache) => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './icon.png'
-      ]);
-    })
-  );
-});
+self.addEventListener('install', () => self.skipWaiting());
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
+  event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener('fetch', () => {});
